dacfa43bae3e87302b484f8bd4cb2f28e93f3a11
Antonio-Jung <antonio_jung@wamazing.jp>
2019-06-11 18:56:38 +0900

    canvas ctx basic

--- app.js
+++ app.js
@@ -1,31 +1,42 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
-
-function onMouseMove(event) {
-  const x = event.offsetX;
-  const y = event.offsetY;
-  //console.log(x,y);
-}
 
 function stopPainting() {
   painting = false;
 }
 
-
-function onMouseDown(event) {
+function startPainting() {
   painting = true;
 }
 
-function onMouseUp(event) {
-  stopPainting();
+function onMouseMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+  //console.log(x,y);
+  // path 만들기 시작점
+  if(!painting) {
+    ctx.beginPath(); 
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
+}
+
+function onMouseDown(event) {
+  painting = true;
 }
 
 
 // check 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown); // 클릭했을때 발생하는 이벤트
-  canvas.addEventListener("mouseup", onMouseUp); // 클릭을 뗴었을때 이벤트
+  canvas.addEventListener("mousedown", startPainting); // 클릭했을때 발생하는 이벤트
+  canvas.addEventListener("mouseup", stopPainting); // 클릭을 뗴었을때 이벤트
   canvas.addEventListener("mouseleave", stopPainting) // 클릭 범위를 벗어났을때 이벤트
 }  
