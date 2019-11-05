f0da41691217c3f4898103bded86bdd8b2b0f3c8
Antonio-Jung <antonio_jung@wamazing.jp>
2019-11-05 15:46:07 +0900

    fixed box size

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mode = document.getElementById("jsMode");
 const saveBtn = document.getElementById("jsSave");
 
 const INITIAL_COLOR = "#2c2c2c";
-const CANVAS_SIZE = 540;
+const CANVAS_SIZE = 520;
 
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
